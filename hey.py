0ba9d1f50c396478ee6a857ec890c0123d9310c9
Mr.Chanchlesh sharma <123403985+Chanchleshsharma@users.noreply.github.com>
2023-02-03 22:25:07 +0530

    Update hey.py

--- hey.py
+++ hey.py
@@ -1,1 +1,12 @@
 print("hey")
+class human:
+  def eating (self):
+    print("this is eating :")
+    
+class student(human):
+  def study (self):
+    print ("this is study:")
+    
+obj= student()
+obj.study()
+obj.eating()
